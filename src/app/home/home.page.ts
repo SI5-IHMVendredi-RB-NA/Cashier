42e2ae1dd3f0621c50904eab2dd9b9e2d06f3eb0
AzzouziNouamane <nouamane.azzouzi@etu.unice.fr>
2021-02-09 18:53:53 +0100

    added cashier app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Component, OnInit } from '@angular/core';
+import { Commande } from 'src/app/models/Commande';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +9,56 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  data: any;
+  order: Commande;
 
-  constructor() {}
+  constructor(private barcodeScanner: BarcodeScanner, private toast: ToastController) { }
+
+  ngOnInit() {
+  }
+
+  scan() {
+    this.data = null;
+    this.barcodeScanner.scan().then(barcodeData => {
+      console.log('Barcode data', barcodeData);
+      this.data = barcodeData;
+      console.log(this.data);
+      this.order = JSON.parse(barcodeData.text);
+      console.log(this.order);
+    }).catch(err => {
+      console.log('Error', err);
+    });
+  }
+
+  getRepasFromOrder()
+  {
+    return this.order.repas.nom + ', Prix : ' + this.order.repas.prix + '€';
+  }
+
+  getClientFromOrder() 
+  {
+    return  'Client : ' + this.order.client.nom + '\n' + ' Balance : ' + this.order.client.balance;
+  }
+
+  confirm()
+  {
+    this.order.client.balance -= this.order.repas.prix;
+    this.presentToast();
+  }
+
+  async presentToast() {
+    const toast = await this.toast.create({
+      message: 'Commande payée',
+      duration: 7000,
+      buttons:
+      [
+        {
+          text: 'OK',
+          role: 'cancel'
+        }
+      ]
+    });
+    toast.present();
+  }
 
 }
